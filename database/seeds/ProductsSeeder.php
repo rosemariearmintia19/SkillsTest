0b296e130ce3648963bfd3bbf8ec04cb66f3a872
rosemarie armintia <rosemariearmintia05@gmail.com>
2021-12-29 15:17:29 +0900

    seed product

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -11,9 +11,15 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-        Product::create([
-            'name' => '30ml Alcoholder',
-            'stock' => '30'
-        ]);
+
+        $items = [            
+                ['name' => '30ml Alcoholder','stock' => '30'],
+                ['name' => '50ml Alcoholder','stock' => '30'], 
+                ['name' => '60ml Alcoholder','stock' => '30']
+        ];
+        foreach ($items as $item) {
+            Product::create($item);
+        }
+        
     }
 }
